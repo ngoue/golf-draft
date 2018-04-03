0f8cbe3ab33d1523fcadc239c6f3bd2552601be7
Jordan Gardner <jordanthomasg@gmail.com>
2018-04-03 11:39:59 -0400

    Update model to allow empty player fields

--- leaderboard/models.py
+++ leaderboard/models.py
@@ -3,9 +3,9 @@ from django.db import models
 # Create your models here.
 class Drafter(models.Model):
     name = models.CharField(max_length=250)
-    player1 = models.IntegerField()
-    player2 = models.IntegerField()
-    player3 = models.IntegerField()
+    player1 = models.IntegerField(blank=True, null=True)
+    player2 = models.IntegerField(blank=True, null=True)
+    player3 = models.IntegerField(blank=True, null=True)
 
     def __str__(self):
         return self.name
